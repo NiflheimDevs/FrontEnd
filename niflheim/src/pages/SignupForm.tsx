7212d98279c77c58edf2a3d59bdaac6a03834a63
Amir <Amirgmali.com@gmail.com>
2025-03-07 00:48:44 +0330

    fix(signup):hover and validation

--- niflheim/src/pages/SignupForm.tsx
+++ niflheim/src/pages/SignupForm.tsx
@@ -6,6 +6,91 @@ const SignupForm = () => {
   
   const [showPassword2, setShowPassword2] = useState(false);
 
+  const [isValidUser, setIsValidUser] = useState<boolean | null>(null); 
+  const [isValidPhone, setIsValidPhone] = useState<boolean | null>(null); 
+  const [isValidPass, setIsValidPass] = useState<boolean | null>(null); 
+  const [isValidPassRepeat, setIsValidPassRepeat] = useState<boolean | null>(null); 
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+
+  const validateUsername = (value) => {
+    if (!value) {
+      return false;
+    } else if (value.length < 8) {
+      return false;
+    } else if (value.length > 16) {
+      return false;
+    }  else if (!/\d/.test(value)) {
+      return false;
+    } else if (!/[A-Z]/.test(value)) {
+      return false;
+    } else if (!/[a-z]/.test(value)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  
+  const validatePassword = (value) => {
+    if (!value) {
+      return false;
+    } else if (value.length < 8) {
+      return false;
+    } else if (!/\d/.test(value)) {
+      return false;
+    } else if (!/[A-Z]/.test(value)) {
+      return false;
+    } else if (!/[a-z]/.test(value)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const validatePasswordRepeat = (value) => {
+    if (value != password) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const validatePhone = (value) => {
+    if (!value) {
+      return false;
+    } else if (!/^09[0-9]{9}$/.test(value)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPassword(value);
+    setIsValidPass(validatePassword(value));
+  };
+
+  const handleChangePassRepeat = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPasswordRepeat(value);
+    setIsValidPassRepeat(validatePasswordRepeat(value));
+  };
+  
+  const handleChangeUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUsername(value);
+    setIsValidUser(validateUsername(value));
+  };
+
+  const handleChangePhone = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPhone(value);
+    setIsValidPhone(validatePhone(value));
+  };
+
   return (
     <div className="md:w-full sm:w-8/10">
       <div className="text-2xl font-semibold font-[vazirmatn] text-center mb-4">
@@ -18,26 +103,40 @@ const SignupForm = () => {
         <input
           type="text"
           placeholder="نام کاربری"
-          className="w-full bg-[#E5E5E5] py-1.75 px-3 rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]"
+          value={username}
+          onChange={handleChangeUser}
+          className={`w-full bg-[#E5E5E5] py-1.75 px-3 focus:ring-3 focus:outline-none focus:bg-white hover:bg-white transition duration-200 ease-in-out rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]${isValidUser === false
+            ? "focus:ring-3 ring-red-500"
+            : isValidUser === true
+            ? "focus:ring-3 ring-green-500"
+            : "focus:ring-3 ring-gray-300"
+            }`}
         />
         <object
           data="/src/assets/User.svg"
           type="image/svg+xml"
-          className="w-6.5 h-6.5 absolute left-3 top-1/2 transform -translate-y-1/2"
+          className="w-6.5 h-6.5 pointer-events-none absolute left-3 top-1/2 transform -translate-y-1/2"
         />
       </div>
 
-      {/* فیلد ایمیل */}
+      {/* فیلد تلفن‌همراه */}
       <div className="relative w-full">
         <input
           type="text"
           placeholder="تلفن‌همراه"
-          className="w-full bg-[#E5E5E5] py-1.75 px-3 rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]"
+          value={phone}
+          onChange={handleChangePhone}
+          className={`w-full bg-[#E5E5E5] py-1.75 px-3 focus:ring-3 focus:outline-none focus:bg-white hover:bg-white transition duration-200 ease-in-out rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]${isValidPhone === false
+            ? "focus:ring-3 ring-red-500"
+            : isValidPhone === true
+            ? "focus:ring-3 ring-green-500"
+            : "focus:ring-3 ring-gray-300"
+            }`}
         />
         <object
           data="/src/assets/Phone.svg"
           type="image/svg+xml"
-          className="w-6.5 h-6.5 absolute left-3 top-1/2 transform -translate-y-1/2"
+          className="w-6.5 h-6.5 pointer-events-none absolute left-3 top-1/2 transform -translate-y-1/2"
         />
       </div>
 
@@ -46,9 +145,16 @@ const SignupForm = () => {
         <input
           type={showPassword1 ? "text" : "password"} 
           placeholder="رمز عبور"
-          className="w-full bg-[#E5E5E5] py-1.75 px-3 rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]"
+          value={password}
+          onChange={handleChangePass}
+          className={`w-full bg-[#E5E5E5] py-1.75 px-3 focus:ring-3 focus:outline-none focus:bg-white hover:bg-white transition duration-200 ease-in-out rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]${isValidPass === false
+            ? "focus:ring-3 ring-red-500"
+            : isValidPass === true
+            ? "focus:ring-3 ring-green-500"
+            : "focus:ring-3 ring-gray-300"
+            }`}
         />
-        <button onClick={() => setShowPassword1(!showPassword1)} className="cursor-pointer hover:scale-110 absolute left-3 top-1/2 transform -translate-y-1/2">
+        <button onClick={() => setShowPassword1(!showPassword1)} className="cursor-pointer transition duration-200 ease-in-out hover:scale-110 absolute left-3 top-1/2 transform -translate-y-1/2">
           <object
             data={showPassword1 ? "/src/assets/Eye_off.svg" : "/src/assets/Eye.svg"} 
             type="image/svg+xml"
@@ -62,9 +168,16 @@ const SignupForm = () => {
         <input
           type={showPassword2 ? "text" : "password"} 
           placeholder="تکرار رمز عبور"
-          className="w-full bg-[#E5E5E5] py-1.75 px-3 rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]"
+          value={passwordRepeat}
+          onChange={handleChangePassRepeat}
+          className={`w-full bg-[#E5E5E5] py-1.75 px-3 focus:ring-3 focus:outline-none focus:bg-white hover:bg-white transition duration-200 ease-in-out rounded-[18px] my-2 placeholder-black text-right text-[20px] text-black font-[vazirmatn]${isValidPassRepeat === false
+            ? "focus:ring-3 ring-red-500"
+            : isValidPassRepeat === true
+            ? "focus:ring-3 ring-green-500"
+            : "focus:ring-3 ring-gray-300"
+            }`}
         />
-        <button onClick={() => setShowPassword2(!showPassword2)} className="cursor-pointer hover:scale-110 absolute left-3 top-1/2 transform -translate-y-1/2">
+        <button onClick={() => setShowPassword2(!showPassword2)} className="cursor-pointer transition duration-200 ease-in-out hover:scale-110 absolute left-3 top-1/2 transform -translate-y-1/2">
           <object
             data={showPassword2 ? "/src/assets/Eye_off.svg" : "/src/assets/Eye.svg"} 
             type="image/svg+xml"
@@ -74,7 +187,7 @@ const SignupForm = () => {
       </div>
 
       {/* دکمه تایید و ادامه */}
-      <button className="w-full cursor-pointer rounded-[20px] mt-5 bg-[#3A7D44] shadow-[0_4px_10px_rgba(0,0,0,0.2)] py-3 hover:bg-green-600 transition">
+      <button className="w-full cursor-pointer transition duration-200 ease-in-out rounded-[20px] mt-5 bg-[#3A7D44] shadow-[0_4px_10px_rgba(0,0,0,0.2)] py-3 hover:bg-green-600">
         <p className="text-white font-[vazirmatn] font-extralight">
           تایید و ادامه
         </p>
