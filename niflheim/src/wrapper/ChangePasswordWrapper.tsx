7081b6a4baa023288720b64a944446f7fa243901
Amir4163 <118739694+Amir4163@users.noreply.github.com>
2025-03-18 19:17:10 +0330

    Feature sign up api (#26)
    * feat(SignUp API):add api to backend
    
    * fix(Notification): modify style
    
    * fix(Error): add function to map errors
    
    * fix(Session): save mobile number
    
    * fix(mobileverify): style
    
    * feat(API):add axios setup
    
    * fix(signupform): success notify
    
    * fix(mobileverify): sessions and api
    
    * fix(signupslice): rename and add fields
    
    * fix(store): routing
    
    * fix(Login/Verify): ADD APIs
    
    * fix(auth):modify api and session
    
    * fix(store):modify session
    
    * fix(packages): install package
    
    * feat(svg):add svg
    
    * fix(api):modify api
    
    * fix(wrapper):changepass direct access avoidance
    
    * fix(change/forget pass): modify apis
    
    * fix(change pass):modify api
    
    * fix(store): profileReducer
    
    * fix(forgetpass):session id
    
    * fix(api): login/signup/forgetpass apis and sessions
    
    ---------
    
    Co-authored-by: Amir <Amirgmali.com@gmail.com>

--- niflheim/src/wrapper/ChangePasswordWrapper.tsx
+++ niflheim/src/wrapper/ChangePasswordWrapper.tsx
@@ -1,18 +1,16 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 export default function MobileVerifyWrapper({children}) {
-    const {mobileSession} = useSelector((state: any) => state.auth)
-    const {Phone} = useSelector((state: any) => state.auth)
-
+    const {canChangePassword} = useSelector((state: any) => state.auth)
     const navigate = useNavigate();
 
     useEffect(()=>{
-        if(!mobileSession || !Phone)
+        if(!canChangePassword)
             navigate("/auth");
     }, [])
 
-    if(mobileSession)
+    if(canChangePassword)
     {
         return <> {children}</>;
     }
