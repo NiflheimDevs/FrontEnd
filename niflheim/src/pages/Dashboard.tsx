b41adf200c9075ea83b5ec23339c894658546841
Amir <Amirgmali.com@gmail.com>
2025-03-20 20:28:17 +0330

    fix(dashboard/changepass/header):fix styles

--- niflheim/src/pages/Dashboard.tsx
+++ niflheim/src/pages/Dashboard.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
   return (
     <div className="flex h-fit bg-[#F7F7F7]" dir="rtl">
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <main className="flex-1 flex flex-col pt-16 pr-4 md:pr-24">
+      <main className="flex-1 flex flex-col pt-16 md:pr-24 sm:pr-24 pr-0">
         <Header toggleSidebar={toggleSidebar} />
         <ProfileForm />
       </main>
