65b57ea64db5de7057b47985be52c663dba30a20
Amir4163 <118739694+Amir4163@users.noreply.github.com>
2025-03-20 20:33:12 +0330

    Feature profile (#33)
    * feat(packages): add redux-persist
    
    * feat(profile slice) : local storage for data
    
    * fix(dashboard/changepass/header):fix styles
    
    * fix(sidebar):fix nav link
    
    * fix(profile): new inputs, styles and storing data
    
    * fix(routing):profile routing
    
    ---------
    
    Co-authored-by: Amir <Amirgmali.com@gmail.com>

--- niflheim/src/pages/Dashboard.tsx
+++ niflheim/src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Sidebar from "../Components/DashboardComp/Sidebar";
 import Header from "../Components/DashboardComp/Header";
-import ProfileForm from "../Components/DashboardComp/ProfileForm";
 import React from "react";
 
 export default function Dashboard() {
@@ -14,9 +13,8 @@ export default function Dashboard() {
   return (
     <div className="flex h-fit bg-[#F7F7F7]" dir="rtl">
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <main className="flex-1 flex flex-col pt-16 pr-4 md:pr-24">
+      <main className="flex-1 flex flex-col pt-16 md:pr-24 sm:pr-24 pr-0">
         <Header toggleSidebar={toggleSidebar} />
-        <ProfileForm />
       </main>
     </div>
   );
--- niflheim/src/pages/Profile.tsx
+++ niflheim/src/pages/Profile.tsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
+import Sidebar from "../Components/DashboardComp/Sidebar";
+import Header from "../Components/DashboardComp/Header";
+import ProfileForm from "../Components/DashboardComp/ProfileForm";
 import React from "react";
-import { Link } from "react-router-dom";
 
-const Profile = () => {
+export default function Profile() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-4">Profile Page</h1>
-      
-      <Link to="/dashboard">
-        <button className="bg-blue-500 px-6 py-2 rounded-md hover:bg-blue-600 transition">
-          Back to Dashboard
-        </button>
-      </Link>
+    <div className="flex h-fit bg-[#F7F7F7]" dir="rtl">
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <main className="flex-1 flex flex-col pt-16 md:pr-24 sm:pr-24 pr-0">
+        <Header toggleSidebar={toggleSidebar} />
+        <ProfileForm />
+      </main>
     </div>
   );
-};
-
-export default Profile;
+}
