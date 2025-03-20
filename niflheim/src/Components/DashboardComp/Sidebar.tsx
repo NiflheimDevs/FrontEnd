1347597b2b25e0cf5f2c95088f914d85330b4835
Amir <Amirgmali.com@gmail.com>
2025-03-20 20:32:47 +0330

    fix(routing):profile routing

--- niflheim/src/Components/DashboardComp/Sidebar.tsx
+++ niflheim/src/Components/DashboardComp/Sidebar.tsx
@@ -126,7 +126,7 @@ export default function Sidebar({ isSidebarOpen, toggleSidebar }: any) {
             {isProfileOpen && isHovered && (
               <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-lg shadow-lg z-50">
                 <Link
-                  to="/profile/edit"
+                  to="/profile"
                   className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
                 >
                   ویرایش پروفایل
--- niflheim/src/pages/Dashboard.tsx
+++ niflheim/src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Sidebar from "../Components/DashboardComp/Sidebar";
 import Header from "../Components/DashboardComp/Header";
-import ProfileForm from "../Components/DashboardComp/ProfileForm";
 import React from "react";
 
 export default function Dashboard() {
@@ -16,7 +15,6 @@ export default function Dashboard() {
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <main className="flex-1 flex flex-col pt-16 md:pr-24 sm:pr-24 pr-0">
         <Header toggleSidebar={toggleSidebar} />
-        <ProfileForm />
       </main>
     </div>
   );
