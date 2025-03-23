2036c543bb2a2b72c71cba51c9283a1ebec6ae50
ParsaSamiei <117802212+ParsaSamiei@users.noreply.github.com>
2025-03-23 13:33:06 +0330

    Feat (sidebar): activeMode

--- niflheim/src/Components/DashboardComp/Sidebar.tsx
+++ niflheim/src/Components/DashboardComp/Sidebar.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { MdArrowDropDown } from "react-icons/md";
 import dashboard from "@/assets/Dashboard/Exclude.svg";
 import projects from "@/assets/Dashboard/PencilSquare.svg";
@@ -14,7 +14,8 @@ export default function Sidebar({ isSidebarOpen, toggleSidebar }: any) {
   const navigate = useNavigate();
   const [isProjectsOpen, setIsProjectsOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
-  const [isHovered, setIsHovered] = useState(false); // New state to track hover
+  const [isHovered, setIsHovered] = useState(false);
+  const location = useLocation();
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -34,67 +35,67 @@ export default function Sidebar({ isSidebarOpen, toggleSidebar }: any) {
     }
   };
 
-  // Handle mouse enter
   const handleMouseEnter = () => {
     setIsHovered(true);
   };
 
-  // Handle mouse leave
   const handleMouseLeave = () => {
     setIsHovered(false);
-    setIsProjectsOpen(false); // Close projects dropdown
-    setIsProfileOpen(false); // Close profile dropdown
+    setIsProjectsOpen(false);
+    setIsProfileOpen(false);
+  };
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
   };
 
   return (
     <aside
       className={`fixed top-19 right-0 rounded-tl-3xl rounded-bl-3xl h-[calc(100vh-4rem)] bg-[#D4D4D4] p-5 shadow-sm transition-all duration-300 z-50
-        ${
-          isSidebarOpen ? "w-48" : "w-20"
-        } sm:w-20 sm:hover:w-48 w-full group flex flex-col
+        ${isSidebarOpen ? "w-48" : "w-20"} sm:w-20 sm:hover:w-48 w-full group flex flex-col
         ${isSidebarOpen ? "block" : "hidden"} sm:block`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <nav className="flex flex-col justify-between h-full">
-        {/* Top Section */}
         <div className="mt-5 space-y-5 w-full items-center">
           <Link
             to="/dashboard"
-            className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/dashboard") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
           >
             <img src={dashboard} alt="Dashboard" className="w-6 h-6" />
-            <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+            <span className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/dashboard") ? "text-black" : "text-gray-800"}`}>
               داشبورد
             </span>
+            {isActive("/dashboard") && <div className="absolute left-0 w-1 h-full bg-blue-400"></div>}
           </Link>
 
-          {/* Projects Section with Click Dropdown */}
           <div className="relative">
             <button
               onClick={toggleProjectsDropdown}
-              className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/myprojects") || isActive("/myprojects/active") || isActive("/myprojects/completed") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
             >
               <img src={projects} alt="Projects" className="w-6 h-6" />
-              <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+              <span className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/myprojects") || isActive("/myprojects/active") || isActive("/myprojects/completed") ? "text-black" : "text-gray-800"}`}>
                 پروژه ها
               </span>
               <MdArrowDropDown className="absolute right-10 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800" />
             </button>
-            {/* Dropdown Menu for Projects - Visible on Click and Hover */}
             {isProjectsOpen && isHovered && (
               <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-lg shadow-lg z-50">
                 <Link
                   to="/myprojects/active"
-                  className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+                  className={`block px-4 py-2 hover:bg-gray-300 ${isActive("/myprojects/active") || isActive("/myprojects") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
                 >
                   پروژه‌های فعال
+                  {(isActive("/myprojects/active") || isActive("/myprojects")) && <div className="absolute left-0 w-1 h-full bg-blue-400"></div>}
                 </Link>
                 <Link
                   to="/myprojects/completed"
-                  className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+                  className={`block px-4 py-2 hover:bg-gray-300 ${isActive("/myprojects/completed") || isActive("/myprojects") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
                 >
                   پروژه‌های تکمیل شده
+                  {(isActive("/myprojects/completed") || isActive("/myprojects")) && <div className="absolute left-0 w-1 h-full bg-blue-400"></div>}
                 </Link>
               </div>
             )}
@@ -102,40 +103,42 @@ export default function Sidebar({ isSidebarOpen, toggleSidebar }: any) {
 
           <Link
             to="/wallet"
-            className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/wallet") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
           >
             <img src={Wallet} alt="Wallet" className="w-6 h-6" />
-            <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+            <span className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/wallet") ? "text-black" : "text-gray-800"}`}>
               کیف پول
             </span>
+            {isActive("/wallet") && <div className="absolute left-0 w-1 h-full bg-blue-400"></div>}
           </Link>
 
-          {/* Profile Section with Click Dropdown */}
           <div className="relative">
             <button
               onClick={toggleProfileDropdown}
-              className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/profile") || isActive("/profile/edit") || isActive("/changepass") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
             >
               <img src={profile} alt="Profile" className="w-6 h-6" />
-              <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+              <span className={` absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/profile") || isActive("/profile/edit") || isActive("/changepass") ? "text-black" : "text-gray-800"}`}>
                 پروفایل
               </span>
+              {isActive("/profile") && <div className="absolute left-0 w-1 h-full bg-blue-400"></div>}
+
               <MdArrowDropDown className="absolute right-10 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800" />
             </button>
-            {/* Dropdown Menu for Profile - Visible on Click and Hover */}
             {isProfileOpen && isHovered && (
               <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-lg shadow-lg z-50">
                 <Link
                   to="/profile"
-                  className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+                  className={`block px-4 py-2 hover:bg-gray-300 ${isActive("profile") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
                 >
                   ویرایش پروفایل
                 </Link>
                 <Link
                   to="/changepass"
-                  className="block px-4 py-2 text-gray-800 hover:bg-gray-300"
+                  className={`block px-4 py-2 hover:bg-gray-300 ${isActive("/changepass") ? "font-bold text-black bg-blue-500" : "text-gray-800"}`}
                 >
                   تغییر رمز
+                  {isActive("/changepass") && <div className="absolute left-0 w-1 h-full bg-blue-700"></div>}
                 </Link>
               </div>
             )}
@@ -143,25 +146,26 @@ export default function Sidebar({ isSidebarOpen, toggleSidebar }: any) {
 
           <Link
             to="/messages"
-            className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/messages") ? "font-bold text-black bg-blue-500" : "text-gray-800"}`}
           >
             <img src={messages} alt="Messages" className="w-6 h-6" />
-            <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+            <span className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/messages") ? "text-white" : "text-gray-800"}`}>
               پیام ها
             </span>
+            {isActive("/messages") && <div className="absolute left-0 w-1 h-full bg-blue-700"></div>}
           </Link>
         </div>
 
-        {/* Bottom Section */}
         <div className="space-y-5 w-full items-center">
           <Link
             to="/settings"
-            className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/settings") ? "font-bold text-black bg-blue-500" : "text-gray-800"}`}
           >
             <img src={settings} alt="Setting" className="w-6 h-6" />
-            <span className="absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
+            <span className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/settings") ? "text-white" : "text-gray-800"}`}>
               تنظیمات
             </span>
+            {isActive("/settings") && <div className="absolute left-0 w-1 h-full bg-blue-700"></div>}
           </Link>
           <button
             onClick={handleLogout}
