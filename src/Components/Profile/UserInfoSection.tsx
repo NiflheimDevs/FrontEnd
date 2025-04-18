2bbcbc4d766db0d0111d12c747f7fe7a71fc3316
Amir <Amirgmali.com@gmail.com>
2025-04-18 19:00:10 +0330

    fix(profile):resendOTP button

--- src/Components/Profile/UserInfoSection.tsx
+++ src/Components/Profile/UserInfoSection.tsx
@@ -1,5 +1,5 @@
 import { Image, X } from "lucide-react";
-import { Profile } from "./types";
+import { Profile, useOtpTimer } from "./types";
 import { useNotification } from "../../Notification/NotificationProvider";
 import { PutUserName, PutEmail, PutPhoneSendOtp } from "../../API";
 import React, { useState } from "react";
@@ -47,6 +47,11 @@ export default function UserInfoSection({
     }
   };
 
+  const { timeLeft, setTimeLeft, isScaled, setIsScaled } = useOtpTimer(
+    120,
+    showOtpSection
+  );
+
   const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setLocalProfile((prev) => ({ ...prev, email: value }));
@@ -65,7 +70,8 @@ export default function UserInfoSection({
         const phoneData = { phone: localProfile.phoneNumber };
         const codeSession = await PutPhoneSendOtp(phoneData);
         setLocalProfile((prev) => ({ ...prev, SessionID: codeSession }));
-        console.log(codeSession);
+        setTimeLeft(120);
+        setIsScaled(false);
         setShowOtpSection(true);
         notifySuccess("کد تایید ارسال شد");
       } catch (error: any) {
@@ -304,12 +310,12 @@ export default function UserInfoSection({
           />
           <button
             className={`flex items-center gap-2 md:w-[170px] sm:w-[160px] justify-center transition-all duration-200 ease-in-out rounded-[20px] bg-[#3E79DE] py-2.5 text-white shadow-[0_4px_10px_rgba(0,0,0,0.2)] ${
-              changedPhone
-                ? "hover:bg-blue-600 focus:bg-blue-600 focus:shadow-lg cursor-pointer"
-                : "opacity-60"
+              !changedPhone || (showOtpSection && timeLeft > 0)
+                ? "opacity-60"
+                : "hover:bg-blue-600 focus:bg-blue-600 focus:shadow-lg cursor-pointer"
             }`}
             tabIndex={8}
-            disabled={!changedPhone}
+            disabled={!changedPhone || (showOtpSection && timeLeft > 0)}
             onClick={handlePhoneChangeSubmit}
           >
             {!showOtpSection ? (
@@ -352,6 +358,10 @@ export default function UserInfoSection({
         </div>
 
         <OtpSection
+          isScaled={isScaled}
+          setIsScaled={setIsScaled}
+          timeLeft={timeLeft}
+          setTimeLeft={setTimeLeft}
           showOtpSection={showOtpSection}
           setShowOtpSection={setShowOtpSection}
           phoneNumber={localProfile.phoneNumber}
