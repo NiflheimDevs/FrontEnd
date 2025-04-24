83ce79468c7093aef67a48b9d633235046adf009
ali <118683797+alig41382@users.noreply.github.com>
2025-04-24 20:43:28 +0330

    Feat chat (#63)
    * feat :chat_search(tel style)  fix:sidebar
    
    * feat : changes to chat

--- src/Components/DashboardComp/Sidebar.tsx
+++ src/Components/DashboardComp/Sidebar.tsx
@@ -6,10 +6,10 @@ import projects from "@/assets/Dashboard/PencilSquare.svg";
 import profile from "@/assets/Dashboard/PersonCheckFill.svg";
 import Wallet from "@/assets/Dashboard/Credit.svg";
 import messages from "@/assets/Dashboard/Message.svg";
-// import settings from "@/assets/Dashboard/settings.svg";
+import settings from "@/assets/Dashboard/settings.svg"; // Re-added settings
 import exit from "@/assets/Dashboard/DoorOpen.svg";
 import { useNotification } from "../../Notification/NotificationProvider";
-import { logout } from "../../API"; // Adjust the path
+import { logout } from "../../API";
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -24,8 +24,8 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
   const location = useLocation();
 
   const handleLogout = () => {
-    logout(); // Use the imported logout function which handles token removal and navigation
-    notifySuccess("شما خارج شدید"); // Show success notification
+    logout();
+    notifySuccess("شما خارج شدید");
     console.log("User logged out");
   };
 
@@ -63,15 +63,19 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <nav className="flex flex-col justify-between h-full overflow-scroll">
+      <nav className="flex flex-col justify-between h-full">
         <div className="mt-5 space-y-[3.5vh] w-full items-center">
           <Link
             to="/dashboard"
-            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/dashboard") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400 ${
+              isActive("/dashboard") ? "font-bold text-black bg-blue-200" : "text-gray-800"
+            }`}
           >
             <img src={dashboard} alt="Dashboard" className="w-6 h-6" />
             <span
-              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/dashboard") ? "text-black" : "text-gray-800"}`}
+              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${
+                isActive("/dashboard") ? "text-black" : "text-gray-800"
+              }`}
             >
               داشبورد
             </span>
@@ -83,18 +87,30 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
           <div className="relative">
             <button
               onClick={toggleProjectsDropdown}
-              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/myprojects") || isActive("/myprojects/active") || isActive("/myprojects/completed") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400 ${
+                isActive("/myprojects") ||
+                isActive("/myprojects/active") ||
+                isActive("/myprojects/completed")
+                  ? "font-bold text-black bg-blue-200"
+                  : "text-gray-800"
+              }`}
             >
               <img src={projects} alt="Projects" className="w-6 h-6" />
               <span
-                className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/myprojects") || isActive("/myprojects/active") || isActive("/myprojects/completed") ? "text-black" : "text-gray-800"}`}
+                className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${
+                  isActive("/myprojects") ||
+                  isActive("/myprojects/active") ||
+                  isActive("/myprojects/completed")
+                    ? "text-black"
+                    : "text-gray-800"
+                }`}
               >
                 پروژه ها
               </span>
               <MdArrowDropDown className="absolute right-10 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800" />
             </button>
             {isProjectsOpen && isHovered && (
-              <div className="absolute right-0 mt-2 w-full bg-gray-200 rounded-lg shadow-lg z-50">
+              <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-lg shadow-lg z-50">
                 <Link
                   to="/myprojects"
                   className="block px-4 py-2 text-gray-800 hover:bg-gray-300 hover:rounded-lg"
@@ -119,11 +135,15 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
 
           <Link
             to="/wallet"
-            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/wallet") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400 ${
+              isActive("/wallet") ? "font-bold text-black bg-blue-200" : "text-gray-800"
+            }`}
           >
             <img src={Wallet} alt="Wallet" className="w-6 h-6" />
             <span
-              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/wallet") ? "text-black" : "text-gray-800"}`}
+              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${
+                isActive("/wallet") ? "text-black" : "text-gray-800"
+              }`}
             >
               کیف پول
             </span>
@@ -135,31 +155,42 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
           <div className="relative">
             <button
               onClick={toggleProfileDropdown}
-              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/profile") || isActive("/profile/edit") || isActive("/changepass") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+              className={`cursor-pointer relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400 ${
+                isActive("/profile") || isActive("/profile/edit") || isActive("/changepass")
+                  ? "font-bold text-black bg-blue-200"
+                  : "text-gray-800"
+              }`}
             >
               <img src={profile} alt="Profile" className="w-6 h-6" />
               <span
-                className={` absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/profile") || isActive("/profile/edit") || isActive("/changepass") ? "text-black" : "text-gray-800"}`}
+                className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${
+                  isActive("/profile") || isActive("/profile/edit") || isActive("/changepass")
+                    ? "text-black"
+                    : "text-gray-800"
+                }`}
               >
                 پروفایل
               </span>
               {isActive("/profile") && (
                 <div className="absolute left-0 w-1 h-full bg-blue-400"></div>
               )}
-
               <MdArrowDropDown className="absolute right-10 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800" />
             </button>
             {isProfileOpen && isHovered && (
-              <div className="absolute right-0 mt-2 w-full bg-gray-200 rounded-lg shadow-lg z-50">
+              <div className="absolute right-0 mt-2 w-48 bg-gray-200 rounded-lg shadow-lg z-50">
                 <Link
                   to="/profile"
-                  className={`block px-4 py-2 hover:bg-gray-300 hover:rounded-lg ${isActive("profile") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+                  className={`block px-4 py-2 hover:bg-gray-300 hover:rounded-lg ${
+                    isActive("/profile") ? "font-bold text-black bg-blue-200" : "text-gray-800"
+                  }`}
                 >
                   ویرایش پروفایل
                 </Link>
                 <Link
                   to="/changepass"
-                  className={`block px-4 py-2 hover:bg-gray-300 hover:rounded-lg ${isActive("/changepass") ? "font-bold text-black bg-blue-500" : "text-gray-800"}`}
+                  className={`block px-4 py-2 hover:bg-gray-300 hover:rounded-lg ${
+                    isActive("/changepass") ? "font-bold text-black bg-blue-500" : "text-gray-800"
+                  }`}
                 >
                   تغییر رمز
                   {isActive("/changepass") && (
@@ -172,11 +203,15 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
 
           <Link
             to="/chat"
-            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300 ${isActive("/chat") ? "font-bold text-black bg-blue-200" : "text-gray-800"}`}
+            className={`relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400 ${
+              isActive("/chat") ? "font-bold text-black bg-blue-200" : "text-gray-800"
+            }`}
           >
             <img src={messages} alt="chat" className="w-6 h-6" />
             <span
-              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${isActive("/chat") ? "text-black" : "text-gray-800"}`}
+              className={`absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 ${
+                isActive("/chat") ? "text-black" : "text-gray-800"
+              }`}
             >
               پیام ها
             </span>
@@ -186,7 +221,7 @@ export default function Sidebar({ isSidebarOpen }: SidebarProps) {
           </Link>
           <button
             onClick={handleLogout}
-            className="flex relative items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-300"
+            className="relative flex items-center w-full p-2 rounded hover:bg-gray-200 transition-all duration-400"
           >
             <img src={exit} alt="exit" className="w-6 h-6" />
             <span className="cursor-pointer absolute right-14 sm:opacity-0 sm:group-hover:opacity-100 transition-opacity duration-300 text-gray-800">
