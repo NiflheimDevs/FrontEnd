64bcc99b75a541f1581039a40e2d566938e32d11
ali <118683797+alig41382@users.noreply.github.com>
2025-04-26 11:37:57 +0330

    Feat chat (#65)
    * feat :chat_search(tel style)  fix:sidebar
    
    * feat : changes to chat
    
    * Test for push
    
    * test
    
    * chore: trigger redeploy
    
    * test
    
    * fix : sidebar unused var

--- src/Components/DashboardComp/Sidebar.tsx
+++ src/Components/DashboardComp/Sidebar.tsx
@@ -6,7 +6,6 @@ import projects from "@/assets/Dashboard/PencilSquare.svg";
 import profile from "@/assets/Dashboard/PersonCheckFill.svg";
 import Wallet from "@/assets/Dashboard/Credit.svg";
 import messages from "@/assets/Dashboard/Message.svg";
-import settings from "@/assets/Dashboard/settings.svg"; // Re-added settings
 import exit from "@/assets/Dashboard/DoorOpen.svg";
 import { useNotification } from "../../Notification/NotificationProvider";
 import { logout } from "../../API";
