7168087dcdc1fcce28a292d3e5bed839edc0e924
Amir <Amirgmali.com@gmail.com>
2025-05-01 11:09:21 +0330

    feat(api): add id

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -88,7 +88,8 @@ const MyProjects = () => {
       const offset = (page - 1) * projectsPerPage;
       const data: { projects: Project[]; total: number } = await getUserProject(
         offset,
-        projectsPerPage
+        projectsPerPage,
+        0
       );
 
       console.log(
