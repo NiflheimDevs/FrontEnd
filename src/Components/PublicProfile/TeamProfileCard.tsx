b95c4e2f2a867fdbd4299f30eabdcd7d48ebd459
Amir <Amirgmali.com@gmail.com>
2025-05-19 10:35:12 +0330

    fix(public profile): image

--- src/Components/PublicProfile/TeamProfileCard.tsx
+++ src/Components/PublicProfile/TeamProfileCard.tsx
@@ -257,7 +257,7 @@ const TeamProfileCard = ({ data, localcolor }: TeamProfileCardProps) => {
       {/* Improved Image Modal */}
       {isModalOpen && selectedImage && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-300"
+          className="fixed inset-0 backdrop-blur-xs bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-300"
           onClick={closeModal}
           role="dialog"
           aria-labelledby="modal-title"
@@ -268,7 +268,7 @@ const TeamProfileCard = ({ data, localcolor }: TeamProfileCardProps) => {
             onClick={(e) => e.stopPropagation()}
           >
             <button
-              className="absolute cursor-pointer top-[-2rem] right-[-2rem] text-gray-200 hover:text-white bg-gray-800 rounded-full p-2 shadow-md transition-transform hover:scale-110 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              className="absolute cursor-pointer top-[1rem] right-[3rem] text-gray-200 hover:text-white bg-gray-800 rounded-full p-2 shadow-md transition-transform hover:scale-110 focus:outline-none focus:ring-2 focus:ring-gray-400"
               onClick={closeModal}
               aria-label="بستن تصویر"
             >
